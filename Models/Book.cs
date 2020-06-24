ba1e24462723daa1dfa4b00365baee47b8749a49
dvfleet413 <dvfleet413@gmail.com>
2020-06-24 11:14:05 -0400

    initial commit

---
+++ Models/Book.cs
@@ -0,0 +1,8 @@
+public class Book 
+{
+    // define properties of the Book obj
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Chapters { get; set; }
+    public bool IsCurrentBook { get; set; }
+}
