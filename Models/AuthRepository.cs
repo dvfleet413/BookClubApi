a3b54ed056258d3ebc469a37beb756e353ac05fb
dvfleet413 <dvfleet413@gmail.com>
2020-06-26 13:04:32 -0400

    implemented Register method in AuthRepository

--- Models/AuthRepository.cs
+++ Models/AuthRepository.cs
@@ -4,14 +4,36 @@ namespace BookClubApi.Models
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly AppDbContext _appDbContext;
+        public AuthRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
         public Task<User> Login(string username, string pasword)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            byte[] hashedPassword, passwordSalt;
+            CreatePasswordHash(password, out hashedPassword, out passwordSalt);
+
+            user.HashedPassword = hashedPassword;
+            user.PasswordSalt = passwordSalt;
+
+            await _appDbContext.Users.AddAsync(user);
+            await _appDbContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] hashedPassword, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512()){
+                passwordSalt = hmac.Key;
+                hashedPassword = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
 
         public Task<bool> UserExists(string username)
