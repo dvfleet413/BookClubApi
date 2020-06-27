a9b04fa20e5c19478bee3c9dc97f8752a715a29b
dvfleet413 <dvfleet413@gmail.com>
2020-06-26 22:42:40 -0400

    added IsActive property to User model

---
+++ Migrations/20200627024102_AddIsActiveColumnToUsers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookClubApi.Migrations
+{
+    public partial class AddIsActiveColumnToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,7 @@ namespace BookClubApi.Models
         public int UserId { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public bool IsActive { get; set; }
         public byte[] PasswordSalt {get; set; }
         public byte[] HashedPassword { get; set;}
     }
