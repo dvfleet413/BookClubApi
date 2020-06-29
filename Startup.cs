d34f6bc81d25d8cc80db9c61ac8c1998d14b1bdb
dvfleet413 <dvfleet413@gmail.com>
2020-06-29 19:55:11 -0400

    containerized app, updated startup class to set connection string based on env

--- Startup.cs
+++ Startup.cs
@@ -40,9 +40,35 @@ namespace BookClubApi
             services.AddScoped<IReadingRepository, ReadingRepository>();
 
 
-            // Add the DbContext and specify to use postres, using the default connection string in appsettings.json
+            // Add the DbContext and specify to use postres, using the default connection string in appsettings.json, or using Heroku environment variable
             services.AddDbContext<AppDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+                {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                    string connStr;
+
+                    if (env == "Development")
+                    {
+                        connStr = Configuration.GetConnectionString("ApplicationDbContext");
+                    }
+                    else
+                    {
+                        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                        // Parse connection URL to connection string for Npgsql
+                        connUrl = connUrl.Replace("postgres://", string.Empty);
+                        var pgUserPass = connUrl.Split("@")[0];
+                        var pgHostPortDb = connUrl.Split("@")[1];
+                        var pgHostPort = pgHostPortDb.Split("/")[0];
+                        var pgDb = pgHostPortDb.Split("/")[1];
+                        var pgUser = pgUserPass.Split(":")[0];
+                        var pgPass = pgUserPass.Split(":")[1];
+                        var pgHost = pgHostPort.Split(":")[0];
+                        var pgPort = pgHostPort.Split(":")[1];
+                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                    }
+                    options.UseNpgsql(connStr);
+                });
+                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +79,7 @@ namespace BookClubApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
